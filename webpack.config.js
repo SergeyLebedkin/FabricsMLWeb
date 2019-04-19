58414145e4253f753d74e37e5af407fb83201f99
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2019-04-19 15:46:28 +0300

    ify-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const BomPlugin = require('webpack-utf8-bom');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -8,26 +7,29 @@ module.exports = {
     entry: {
         main: './src/js/scan.js'
     },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/scan.js'
+    },
     node: {
-        fs: 'empty'
+        fs: 'empty' // need for tiff.js
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
     },
     devtool: 'source-map',
     devServer: {
         watchContentBase: true,
         overlay: true
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/scan.js'
-    },
     module: {
         rules: [{
             test: /\.(ts|tsx)$/,
             use: 'ts-loader'
+        }, {
+            test: /\.js$/,
+            loader: 'ify-loader'
         }]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -35,7 +37,6 @@ module.exports = {
             { from: "src/css", to: "css" },
             //{ from: "src/js/deps", to: "js/deps" }
         ]),
-        new BomPlugin(true),
         new HtmlWebpackPlugin({
             inject: false,
             template: './src/index.html',
