a5c47c4d23ca890f79a1aa4d39c9e6828cdc9aa5
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2019-06-12 11:53:32 +0300

    select page base functionality added

--- src/js/select.js
+++ src/js/select.js
@@ -4,11 +4,9 @@ import { MouseUsageMode } from "./FabricsML/Types/MouseUsageMode";
 import { SelectionInfoType } from "./FabricsML/Types/SelectionInfoType";
 import { SelectionInfoMode } from "./FabricsML/Types/SelectionInfoMode";
 import { ImageInfoAreasEditor } from "./FabricsML/Components/ImageInfoAreasEditor";
-import { ImageInfoIntensityHistViewer } from "./FabricsML/Components/ImageInfoIntensityHistViewer";
 
 let gImageInfoList = [];
 let gImageInfoAreasEditor = null;
-let gImageInfoIntensityHistViewer = null;
 
 ///////////////////////////////////////////////////////////////////////////////
 // EVENTS
@@ -56,12 +54,8 @@ function buttonLoadImageFileClick(event) {
                 }
 
                 // image mask canvas
-                if (!gImageInfoAreasEditor.imageInfo)
+                if (!gImageInfoAreasEditor.imageInfo) {
                     gImageInfoAreasEditor.setImageInfo(event.currentTarget.imageInfo);
-
-                if (!gImageInfoIntensityHistViewer.imageInfo) {
-                    gImageInfoIntensityHistViewer.setImageInfo(event.currentTarget.imageInfo);
-                    updateIntensityInputs();
                     updateResolutionInputs();
                 }
             }
@@ -97,7 +91,6 @@ function selectImagesUpdate() {
     if ((selectedIndex < 0) && (gImageInfoList.length > 0))
         selectedIndex = 0;
     selectImages.selectedIndex = selectedIndex;
-    updateIntensityInputs();
     updateResolutionInputs();
 }
 
@@ -113,82 +106,16 @@ function buttonScaleUpClick(event) {
     scaleFactor.innerText = Math.round(gImageInfoAreasEditor.imageScale * 100) + "%";
 }
 
-// rangeIntensityLowOnChange
-function rangeIntensityLowOnChange(event) {
-    if (gImageInfoIntensityHistViewer.imageInfo) {
-        textIntensityLow.value = rangeIntensityLow.value;
-        gImageInfoIntensityHistViewer.imageInfo.setIntensityLow(rangeIntensityLow.value);
-        gImageInfoIntensityHistViewer.imageInfo.updateHilightCanvasAndIntensity();
-        gImageInfoIntensityHistViewer.drawHistogram();
-        gImageInfoAreasEditor.drawImageInfo();
-    }
-}
-
-// rangeIntensityMediumOnChange
-function rangeIntensityMediumOnChange(event) {
-    if (gImageInfoIntensityHistViewer.imageInfo) {
-        textIntensityMedium.value = rangeIntensityMedium.value;
-        gImageInfoIntensityHistViewer.imageInfo.setIntensityMedium(rangeIntensityMedium.value);
-        gImageInfoIntensityHistViewer.imageInfo.updateHilightCanvasAndIntensity();
-        gImageInfoIntensityHistViewer.drawHistogram();
-        gImageInfoAreasEditor.drawImageInfo();
-    }
-}
-
-// rangeIntensityHighOnChange
-function rangeIntensityHighOnChange(event) {
-    if (gImageInfoIntensityHistViewer.imageInfo) {
-        textIntensityHigh.value = rangeIntensityHigh.value;
-        gImageInfoIntensityHistViewer.imageInfo.setIntensityHigh(rangeIntensityHigh.value);
-        gImageInfoIntensityHistViewer.imageInfo.updateHilightCanvasAndIntensity();
-        gImageInfoIntensityHistViewer.drawHistogram();
-        gImageInfoAreasEditor.drawImageInfo();
-    }
-}
-
 // selectImagesOnChange
 function selectImagesOnChange(event) {
     gImageInfoAreasEditor.setImageInfo(gImageInfoList[selectImages.selectedIndex]);
-    gImageInfoIntensityHistViewer.setImageInfo(gImageInfoList[selectImages.selectedIndex]);
-    updateIntensityInputs();
     updateResolutionInputs();
-}
-
-// buttonSaveFabricsOnClick
-function buttonSaveFabricsOnClick(event) {
-    // base check
-    if (!gImageInfoAreasEditor.imageInfo) return;
-
-    // get file name
-    var filename = gImageInfoAreasEditor.imageInfo.fileRef.name.substr(0, gImageInfoAreasEditor.imageInfo.fileRef.name.lastIndexOf('.'));
-
-    // generate xml string
-    var regionsString = '<?xml version="1.0" encoding="utf-8"?>' + "\r\n";
-    regionsString += "<FabricsMLData>" + "\r\n";
-    regionsString += gImageInfoAreasEditor.imageInfo.toStringXmlNode() + "\r\n";
-    regionsString += "</FabricsMLData>" + "\r\n";
-    regionsString += "<HighResolutionImageData>" + "\r\n";
-    regionsString += "</HighResolutionImageData>" + "\r\n";
-
-    downloadFile(regionsString, filename + ".xml", 'text/plain');
-}
-
-// updateIntensityInputs
-function updateIntensityInputs() {
-    if (gImageInfoIntensityHistViewer.imageInfo) {
-        rangeIntensityLow.value = gImageInfoIntensityHistViewer.imageInfo.intensityLow;
-        rangeIntensityMedium.value = gImageInfoIntensityHistViewer.imageInfo.intensityMedium;
-        rangeIntensityHigh.value = gImageInfoIntensityHistViewer.imageInfo.intensityHigh;
-        textIntensityLow.value = rangeIntensityLow.value;
-        textIntensityMedium.value = rangeIntensityMedium.value;
-        textIntensityHigh.value = rangeIntensityHigh.value;
-    }
 }
 
 // updateResolutionInputs
 function updateResolutionInputs() {
-    if (gImageInfoIntensityHistViewer.imageInfo) {
-        let imageInfo = gImageInfoIntensityHistViewer.imageInfo;
+    if (gImageInfoAreasEditor.imageInfo) {
+        let imageInfo = gImageInfoAreasEditor.imageInfo;
         inputImageDimXpx.value = imageInfo.canvasImage.width;
         inputImageDimYpx.value = imageInfo.canvasImage.height;
         inputImageDimXmm.value = (imageInfo.canvasImage.width * imageInfo.imageResolution).toFixed(5);
@@ -201,52 +128,20 @@ function updateResolutionInputs() {
 // window - onload
 window.onload = (event) => {
     // create ImageInfoAreasEditor
-    gImageInfoIntensityHistViewer = new ImageInfoIntensityHistViewer(histogram_canvas_panel);
-    gImageInfoIntensityHistViewer.drawHistogram();
-
-    // create ImageInfoAreasEditor
     gImageInfoAreasEditor = new ImageInfoAreasEditor(image_canvas_panel);
+    gImageInfoAreasEditor.setMouseUsageMode(MouseUsageMode.DRAG)
     image_canvas_panel.addEventListener("mousemove", (event) => gImageInfoAreasEditor.onMouseMove(event));
     image_canvas_panel.addEventListener("mousedown", (event) => gImageInfoAreasEditor.onMouseDown(event));
     image_canvas_panel.addEventListener("mouseup", (event) => gImageInfoAreasEditor.onMouseUp(event));
-    image_canvas_panel.addEventListener("mouseup", (event) => gImageInfoIntensityHistViewer.drawHistogram());
     document.addEventListener("keydown", (event) => gImageInfoAreasEditor.onKeyDown(event));
 
     // apply events
     buttonLoadImageFile.addEventListener("click", buttonLoadImageFileClick);
 
-    // radios
-    radioDraw.addEventListener("click", event => gImageInfoAreasEditor.setMouseUsageMode(MouseUsageMode.DRAW));
-    radioDrag.addEventListener("click", event => gImageInfoAreasEditor.setMouseUsageMode(MouseUsageMode.DRAG));
-    radioRect.addEventListener("click", event => gImageInfoAreasEditor.setSelectionInfoType(SelectionInfoType.RECT));
-    radioArea.addEventListener("click", event => gImageInfoAreasEditor.setSelectionInfoType(SelectionInfoType.AREA));
-    radioInclude.addEventListener("click", event => gImageInfoAreasEditor.setSelectionInfoMode(SelectionInfoMode.INCLUDE));
-    radioExclude.addEventListener("click", event => gImageInfoAreasEditor.setSelectionInfoMode(SelectionInfoMode.EXCLUDE));
-
-    // checkboxes
-    checkboxShowOriginal.addEventListener("click", event => gImageInfoAreasEditor.setShowOriginalImage(checkboxShowOriginal.checked));
-
-    selectImages.onchange = selectImagesOnChange;
-    rangeIntensityLow.oninput = rangeIntensityLowOnChange;
-    rangeIntensityMedium.oninput = rangeIntensityMediumOnChange;
-    rangeIntensityHigh.oninput = rangeIntensityHighOnChange;
-    buttonSaveFabrics.onclick = buttonSaveFabricsOnClick;
+    // events
     buttonScaleDown.onclick = buttonScaleDownClick;
     buttonScaleUp.onclick = buttonScaleUpClick;
 
-    updateIntensityInputs();
-    updateResolutionInputs();
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// utils
-///////////////////////////////////////////////////////////////////////////////
-
-// downloadFile
-function downloadFile(text, name, type) {
-    var a = document.createElement("a");
-    var file = new Blob([text], { type: type });
-    a.href = URL.createObjectURL(file);
-    a.download = name;
-    a.click();
+    // update info
+    updateResolutionInputs()
 }
